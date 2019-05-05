44c0e11b7e38cb805e05fb57dcda0253c952dde2
clussier <lussierc@allegheny.edu>
2019-05-05 17:11:50 -0400

    Python interface code for displaying tables contents.

--- src/interface.py
+++ src/interface.py
@@ -1,6 +1,8 @@
 """Creates a command-line interface where users can interact with the database."""
 
-def welcomeMessage():
+import sqlite3
+
+def welcome_message():
     """Displays welcome message."""
     print("+-----------------------------------------+")
     print("|    Welcome to the AfricaDB Interface    |")
@@ -13,10 +15,39 @@ def welcomeMessage():
     print(" - Resources: Resource Rent data for countries.")
     print()
 
-def displayTable():
-    """Displays table of a user's choice."""
+def get_table_name():
+    """Gets the name of a table."""
+    table_list = ["HPI", "DevIndicators", "RealGDPGrowth", "Resources"]
+
+    table_name = input()
+
+    while(table_name not in table_list):
+        print("** The entered table does not exist in the database. Please re-enter the name:")
+        table_name = input()
+    return table_name
+
+
+def get_table_data():
+    """Gets table contents."""
+    conn = sqlite3.connect("africaDB.sqlite3") # connect to the database
+
+    print("* Please enter a valid table name to display it's contents:")
+    table_name = get_table_name()
+
+    get_data_command = "SELECT * FROM {A}".format(A = table_name)
+
+    result = conn.execute(get_data_command)
+
+    tables = result.fetchall()
+
+    for i in tables:
+        print(i)
+    conn.close()
 
 
 def main():
     """Manages the running of the interface."""
-    welcomeMessage()
+    welcome_message()
+    get_table_data()
+
+main()
